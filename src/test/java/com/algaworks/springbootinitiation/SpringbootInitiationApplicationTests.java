757ad50de698f44e3890b11c78e0cec3c30f8942
Helio Dourado <heliohdd@yahoo.com.br>
2024-04-05 18:23:50 -0300

    Created project web with MySQL and JPA dependencies

---
+++ src/test/java/com/algaworks/springbootinitiation/SpringbootInitiationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.springbootinitiation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInitiationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
